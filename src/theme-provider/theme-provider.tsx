1ae1ffa470a82e960dddbeedc9d2e09403f39d14
sigarachi <lalalabtv@gmail.com>
2024-03-21 00:56:07 +0300

    feat: deploy

--- src/theme-provider/theme-provider.tsx
+++ src/theme-provider/theme-provider.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
-import { ThemeProvider as NativeThemeProvider } from '@emotion/react';
+import {
+	Global,
+	ThemeProvider as NativeThemeProvider,
+	css,
+} from '@emotion/react';
 import { ecosystemTheme } from './constants';
+import RalewayRegular from '../font/Raleway-Regular.ttf';
 
 export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({
 	children,
 }) => (
-	<NativeThemeProvider theme={ecosystemTheme}>{children}</NativeThemeProvider>
+	<NativeThemeProvider theme={ecosystemTheme}>
+		<Global
+			styles={css`
+				@font-face {
+					font-family: 'Raleway Regular';
+					src: url('${RalewayRegular}') format('ttf');
+				}
+
+				body {
+					font-family: 'Raleway Regular', sans-serif;
+				}
+			`}
+		/>
+		{children}
+	</NativeThemeProvider>
 );
