5407f842cc5635d682dbeed6f384cfd295354bad
sigarachi <lalalabtv@gmail.com>
2024-04-12 09:47:24 +0300

    fix: dropdown option position fix

--- src/dropdown/styles.ts
+++ src/dropdown/styles.ts
@@ -3,6 +3,8 @@ import styled from '@emotion/styled';
 export const StyledSelectWrapper = styled('div')`
 	display: flex;
 	flex-direction: column;
+	width: fit-content;
+	position: relative;
 
 	gap: 8px;
 
@@ -14,10 +16,14 @@ export const StyledSelectWrapper = styled('div')`
 export const StyledOptionsWrapper = styled('div')`
 	height: 100px;
 	width: 100%;
+	margin-top: 10px;
 
 	overflow: auto;
 
 	border-radius: 12px;
+	position: absolute;
+
+	top: 100%;
 
 	-webkit-box-shadow: 0px 30px 60px -20px rgba(0, 0, 0, 0.3);
 	-moz-box-shadow: 0px 30px 60px -20px rgba(0, 0, 0, 0.3);
